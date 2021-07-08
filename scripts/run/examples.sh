ef0661a3099098d4cdb606171913b3d3b51097e6
deeperlearner <b04202035@g.ntu.edu.tw>
2021-07-08 15:50:06 +0800

    Enable bash flags

--- scripts/run/examples.sh
+++ scripts/run/examples.sh
@@ -1,46 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script run train and test for examples
-# bash ./scripts/run/examples.sh $1
-# $1: debug, preprocess, run_all
+usage() { echo "Usage: $0 [-dpr]" 1>&2; exit 1; }
 
 
 # "CONFIG##*/" is the basename of CONFIG
-MODE=$1
-if [ "$MODE" = "debug" ]; then
-    CONFIG="examples/Adult_logistic"
-    EXP="Adult_logistic"
-    RUN_ID="debug"
-    python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID --log_name "optuna.log" --optuna --name $EXP
-    python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/tuned_model/model_best.pth" --run_id $RUN_ID
-elif [ "$MODE" = "preprocess" ]; then
-    python3 preprocess.py
-elif [ "$MODE" = "run_all" ]; then
-    # MNIST_LeNet
-    CONFIG="examples/MNIST_LeNet"
-    EXP="MNIST_LeNet"
-    RUN_ID="0"
-    python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID
-    python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/model/model_best.pth" --run_id $RUN_ID
-
-    # ImageNet_VGG16 (need to download ImageNet dataset)
-    CONFIG="examples/ImageNet_VGG16"
-    EXP="ImageNet_VGG16"
-    RUN_ID="0"
-    python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID
-    # no test data
-    # python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/model/model_best.pth" --run_id $RUN_ID
-
-    # Adult_logistic cv by single-process
-    CONFIG="examples/Adult_logistic"
-    EXP="Adult_logistic"
-    RUN_ID="0"
-    python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID
-    python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/model/model_best.pth" --run_id $RUN_ID
-
-    # Not implemented yet
-    # I'm going to try `import torch.multiprocessing as mp`
-    ## Adult_logistic cv by multi-process
-    #CONFIG="examples/Adult_logistic"
-    #EXP="Adult_logistic"
-    #RUN_ID="1"
-fi
+while getopts "dpr" flag; do
+  case "$flag" in
+    d)  # debug
+      CONFIG="examples/Adult_logistic"
+      EXP="Adult_logistic"
+      RUN_ID="debug"
+      python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID --log_name "optuna.log" --optuna --name $EXP
+      python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/tuned_model/model_best.pth" --run_id $RUN_ID
+      ;;
+    p)  # preprocess
+      python3 preprocess.py
+      ;;
+    r)  # run all
+      # MNIST_LeNet
+      CONFIG="examples/MNIST_LeNet"
+      EXP="MNIST_LeNet"
+      RUN_ID="0"
+      python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID
+      python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/model/model_best.pth" --run_id $RUN_ID
+        
+      # ImageNet_VGG16 (need to download ImageNet dataset)
+      CONFIG="examples/ImageNet_VGG16"
+      EXP="ImageNet_VGG16"
+      RUN_ID="0"
+      python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID
+      # no test data
+      # python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/model/model_best.pth" --run_id $RUN_ID
+        
+      # Adult_logistic cv by single-process
+      CONFIG="examples/Adult_logistic"
+      EXP="Adult_logistic"
+      RUN_ID="0"
+      python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID
+      python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/model/model_best.pth" --run_id $RUN_ID
+        
+      # Not implemented yet
+      # I'm going to try `import torch.multiprocessing as mp`
+      ## Adult_logistic cv by multi-process
+      #CONFIG="examples/Adult_logistic"
+      #EXP="Adult_logistic"
+      #RUN_ID="1"
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
--- scripts/run/run.sh
+++ scripts/run/run.sh
@@ -1,20 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script run train and test
-# bash ./scripts/run/run.sh $1
-# $1: debug, preprocess, run_all
+usage() { echo "Usage: $0 [-dpr]" 1>&2; exit 1; }
 
-MODE=$1
 
-if [ "$MODE" = "debug" ]; then
-    CONFIG="dataset_model"
-    EXP="dataset_model"
-    RUN_ID="debug"
-    python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID --log_name "optuna.log" --optuna --name $EXP
-    python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/tuned_model/model_best.pth" --run_id $RUN_ID
-elif [ "$MODE" = "preprocess" ]; then
-    python3 preprocess.py
-elif [ "$MODE" = "run_all" ]; then
-    CONFIG="dataset_model"
-    EXP="dataset_model"
-    RUN_ID=0
-fi
+# "CONFIG##*/" is the basename of CONFIG
+while getopts "dpr" flag; do
+  case "$flag" in
+    d)  # debug
+      CONFIG="dataset_model"
+      EXP="dataset_model"
+      RUN_ID="debug"
+      python3 mains/train/train.py -c "configs/$CONFIG.json" --run_id $RUN_ID --log_name "optuna.log" --optuna --name $EXP
+      python3 mains/test/test.py -c "saved/$EXP/$RUN_ID/${CONFIG##*/}.json" --resume "saved/$EXP/$RUN_ID/tuned_model/model_best.pth" --run_id $RUN_ID
+      ;;
+    p)  # preprocess
+      python3 preprocess.py
+      ;;
+    r)  # run all
+      CONFIG="dataset_model"
+      EXP="dataset_model"
+      RUN_ID=0
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
